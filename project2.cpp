78bdbe05d2bc348eca6c2f70afb5e7110e63c681
Naseri <m86naseri007@gmail.com>
2025-02-14 11:45:55 +0330

    project2.cpp

---
+++ project2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout << "please enter caracter : ";
+    cin >> n ;
+
+    while (n>0){
+        cout << "n: "<< n << endl;
+        n--;
+    }
+    return 0;
+}
